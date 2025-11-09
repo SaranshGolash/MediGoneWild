b47c1aee26cb6f0b9741f3d6aa145bb8d91e7339
SaranshGolash <129923993+SaranshGolash@users.noreply.github.com>
2025-11-09 16:35:30 +0530

    Updated backend logic

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ const __dirname = dirname(__filename);
 // Set up your PostgreSQL pool
 // We'll need this for the Passport database logic
 const db = new pg.Pool({
-  connectionString: process.env.DATABASE_URL, // Make sure this is in your .env
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 const NUMERIC_OID = 1700;
